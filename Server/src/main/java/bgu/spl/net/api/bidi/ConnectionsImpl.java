fc5a8fa34a956a09c50dc17f1b82beab0ed5d641
GuyTurevich <guy938@gmail.com>
2021-12-27 20:04:45 +0200

    ConnectionsImpl singleton

--- Server/src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
+++ Server/src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
@@ -6,11 +6,25 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionsImpl<T> implements Connections<T> {
 
+    private ConnectionsImpl<T> singleton;
     ConcurrentHashMap<Integer, NonBlockingConnectionHandler <T>> clients;
+
+    public ConnectionsImpl(){
+        clients = new ConcurrentHashMap<Integer,NonBlockingConnectionHandler<T>>();
+    }
+
+    public  ConnectionsImpl getInstance() {
+        if (singleton == null) singleton = new ConnectionsImpl<T>();
+        return singleton;
+    }
 
     @Override
     public boolean send(int connectionId, T msg) {
-        return false;
+        if(!clients.containsKey(connectionId))  return false;
+        else{
+
+        }
+        return false; // should delete after done implementing
     }
 
     @Override
