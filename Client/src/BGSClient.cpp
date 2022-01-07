4b42a4510f92cc686cb57b2025cc6d64242dbb66
NitayM <nitaymaoz6@gmail.com>
2022-01-07 20:11:21 +0200

    fixed LOGOUT added ReadWrite class & header

--- Client/src/BGSClient.cpp
+++ Client/src/BGSClient.cpp
@@ -1,88 +1,12 @@
 #include <stdlib.h>
-#include <connectionHandler.h>
+#include "../include/connectionHandler.h"
 #include <iostream>
 #include <thread>
+#include "../include/ReadWrite.h"
 
 /**
 * This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
 */
-//read from console and sends a message to the server
-bool readKeyBoard(ConnectionHandler *connectionHandler) {
-    while (true) {
-        const short bufsize = 1024;
-        char buf[bufsize];
-        std::cin.getline(buf, bufsize);
-        std::string line(buf);
-
-        std::string operation;
-
-        //get operation from line
-        size_t found = line.find(' ');
-        if (found!=std::string::npos){
-            for (int i = 0; line[i] != ' '; i++) {
-                operation.push_back(line[i]);
-            }
-        }
-        else operation=line;
-
-
-
-        bool delivered;
-
-
-        if (operation == "REGISTER") {
-            delivered = connectionHandler->sendLine(line, 01);
-        }
-        else if (operation=="LOGIN") {
-            delivered = connectionHandler->sendLine(line, 02);
-        }
-        else if (operation == "LOGOUT") {
-            delivered = connectionHandler->sendLine(line, 03);
-        }
-        else if (operation=="FOLLOW") {
-            delivered = connectionHandler->sendLine(line, 04);
-        }
-        else if (operation=="POST") {
-            delivered = connectionHandler->sendLine(line, 05);
-        }
-        else if (operation=="PM") {
-            delivered = connectionHandler->sendLine(line, 06);
-        }
-        else if (operation=="LOGSTAT") {
-            delivered = connectionHandler->sendLine(line, 07);
-        }
-        else if (operation=="STAT") {
-            delivered = connectionHandler->sendLine(line,8);
-        }
-        else if (operation=="BLOCK") {
-            delivered = connectionHandler->sendLine(line, 12);
-        }
-
-        if (!delivered) {
-            std::cout << "Disconnected. Exiting...\n" << std::endl;
-            break;
-        }
-    }
-    return false;
-}
-
-bool readFromSocket(ConnectionHandler* connectionHandler) {
-    while (true) {
-        std::string response;
-        if (!connectionHandler->getLine(response)) {
-            std::cout << "Disconnected. Exiting...\n" << std::endl;
-            break;
-        }
-        response=response.substr(0,response.length()-1);
-        std::cout << response << std::endl;
-
-        if (response==("ACK 3")) {
-            break;
-        }
-    }
-    return false;
-}
-
 int main (int argc, char *argv[]) {
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
@@ -96,11 +20,12 @@ int main (int argc, char *argv[]) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
     }
-
-    std::thread write(readKeyBoard,&connectionHandler);
-    readFromSocket(&connectionHandler);
-//    read.join();
-    write.join();
+    std::mutex mutex;
+    std::condition_variable conditionVariable;
+    ReadWrite readWrite(mutex,&connectionHandler,conditionVariable);
+    std::thread readKeyBoard(&ReadWrite::readKeyBoard,&readWrite);
+    readWrite.readFromSocket();
+    readKeyBoard.join();
 
     return 0;
 }
